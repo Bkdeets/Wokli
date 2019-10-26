7268b6fc428cbdc91d227331f929b64c60382755
Britton <brittonkdeets@gmail.com>
2019-10-26 13:12:57 -0400

    api service

--- src/app/services/dynamo-wrapper.service.ts
+++ src/app/services/dynamo-wrapper.service.ts
@@ -1,14 +1,56 @@
-import { Injectable } from '@angular/core';
+import { Injectable } from "@angular/core";
+import axios, { AxiosRequestConfig, AxiosPromise } from "axios";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root"
 })
 export class DynamoWrapperService {
+  proxyurl = "https://cors-anywhere.herokuapp.com/";
+  URL_BASE = "???????";
+  API_KEY = "";
+  INSTANCE;
 
-  constructor() { }
+  constructor() {
+    this.INSTANCE = axios.create({
+      baseURL: this.proxyurl + this.URL_BASE,
+      timeout: 10000,
+      headers: {}
+    });
+  }
 
-  getArticle(url) {
-    return {};
+  async getArticleByProvider(code) {
+    let endpoint = '/articles?provider=' + code
+    return this.INSTANCE.get(endpoint)
+      .then(function(response) {
+        return {
+          response: response,
+          isError: false
+        };
+      })
+      .catch(function(error) {
+        if (error.response) {
+          // The request was made and the server responded with a status code that falls out of the range of 2xx
+          console.log("Error", error);
+          return {
+            response: error,
+            isError: true
+          };
+        } else if (error.request) {
+          // The request was made but no response was received
+          console.log("Error", error);
+          return {
+            response: error,
+            isError: true
+          };
+        } else {
+          // Something happened in setting up the request that triggered an Error
+          console.log("Error", error.message);
+          return {
+            response: error,
+            isError: true
+          };
+        }
+      });
   }
 
   getTopic(name) {
@@ -22,5 +64,4 @@ export class DynamoWrapperService {
   getTopTopics() {
     return {};
   }
-
 }
